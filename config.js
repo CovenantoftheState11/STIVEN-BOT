9d6221ecb48889b1ba14138dec9c7a73dcf3afab
CovenantoftheState11 <markalsydmark@gmail.com>
2024-09-19 13:21:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["967783179256", 'ستيفن', true], 
-["967783179256", 'ستيفن', true], 
-["436703061270111", '-شيخ⁵١⁷.', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["967738512629", 'ستيفن', true], 
+["967738512629", 'ستيفن', true], 
+["967738512629", '-شيخ⁵١⁷.', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 ]
 
 global.mods = []
